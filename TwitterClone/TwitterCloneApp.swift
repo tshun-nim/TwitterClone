92b32e44f41a690553e319b4fa039875a22faf74
tshun-nim <tajimas0811@gmail.com>
2024-11-12 03:52:01 +0900

    Initial Commit

---
+++ TwitterClone/TwitterCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterCloneApp.swift
+//  TwitterClone
+//
+//  Created by 田島隼也 on 2024/11/12.
+//
+
+import SwiftUI
+
+@main
+struct TwitterCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TwitterCloneTests/TwitterCloneTests.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterCloneTests.swift
+//  TwitterCloneTests
+//
+//  Created by 田島隼也 on 2024/11/12.
+//
+
+import Testing
+@testable import TwitterClone
+
+struct TwitterCloneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
