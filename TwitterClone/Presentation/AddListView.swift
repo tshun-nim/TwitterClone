ea408dbda8f0aae2ee51defe79cd109e8ddfa2e1
tshun-nim <tajimas0811@gmail.com>
2024-11-13 02:09:20 +0900

    24.11.13 微修正

---
+++ TwitterClone/Presentation/AddListView.swift
@@ -0,0 +1,52 @@
+//
+//  AddListView.swift
+//  TwitterClone
+//
+//  Created by 田島隼也 on 2024/11/12.
+//
+
+import SwiftUI
+
+struct AddListView: View {
+    @ObservedObject var viewModel: ListViewModel
+    
+    var body: some View {
+        VStack {
+            TextField("", text: $viewModel.editText)
+                .textFieldStyle(.roundedBorder)
+                .padding()
+            
+            HStack {
+                Spacer()
+                Button(action: {
+                    viewModel.showModal = false
+                }) {
+                    Text("キャンセル")
+                }
+                .buttonStyle(.bordered)
+                Spacer()
+                Button(action: {
+                    let val = Fruit(name: viewModel.editText)
+//                        viewModel.editText = ""
+                    if !viewModel.editText.isEmpty {
+                        Task { @MainActor in
+                            viewModel.editText = ""
+                        }
+                    }
+                    viewModel.fruits.append(val)
+                    viewModel.showModal = false
+                }) {
+                    Text("追加")
+                }
+                .buttonStyle(.borderedProminent)
+                Spacer()
+            }
+            .padding()
+        }
+    }
+}
+
+//#Preview {
+//    @StateObject var viewModel: ListViewModel = .init()
+//    AddListView(viewModel: viewModel)
+//}
