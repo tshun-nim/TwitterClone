d5a117530285e26735597a6e6ef9a6e4f3e9dcfb
tshun-nim <tajimas0811@gmail.com>
2024-12-20 00:15:05 +0900

    24.12.20 SwiftData導入

---
+++ TwitterClone/Application/Usecase/TaskUsecase.swift
@@ -0,0 +1,8 @@
+//
+//  TaskUsercase.swift
+//  TwitterClone
+//
+//  Created by 田島隼也 on 2024/12/16.
+//
+
+import Foundation
---
+++ TwitterClone/Infrustructure/api/TaskRepositryImpl.swift
@@ -0,0 +1,8 @@
+//
+//  TaskRepositryImpl.swift
+//  TwitterClone
+//
+//  Created by 田島隼也 on 2024/12/16.
+//
+
+import Foundation
